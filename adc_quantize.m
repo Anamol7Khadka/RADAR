0b1721e43430fd73c43293279751c655be1fdf7e
Anamol7Khadka <khdanamol@gmail.com>
2026-08-26 20:58:46 +0545

    Fix formatting

--- adc_quantize.m
+++ adc_quantize.m
@@ -1,5 +1,5 @@
-function digital = adc_quantize(analog, vref, n_bits) LSB =
-    vref / (2 ^ n_bits - 1);
-clamped = max(0, min(analog, vref));
-digital = round(clamped / LSB) * LSB;
+function digital = adc_quantize(analog, vref, n_bits)
+    LSB = vref / (2^n_bits - 1);
+    clamped = max(0, min(analog, vref));
+    digital = round(clamped / LSB) * LSB;
 end
